3091bf70f039b2721ff13d041a601747b970c94b
yqchenee <a6330610@gmail.com>
2020-09-23 23:26:35 +0800

    fail

---
+++ app/src/main/java/com/example/cyls_2020fall/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.cyls_2020fall.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
