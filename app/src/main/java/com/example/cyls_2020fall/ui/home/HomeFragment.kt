7031532a19153521317b18cb2e501c07d73a4efe
yqchenee <a6330610@gmail.com>
2020-09-23 23:41:27 +0800

    fix

--- app/src/main/java/com/example/cyls_2020fall/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cyls_2020fall/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.cyls_2020fall.R
 
 class HomeFragment : Fragment() {
@@ -27,5 +28,13 @@ class HomeFragment : Fragment() {
             textView.text = it
         })*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<android.widget.Button>(R.id.button2)
+        button?.setOnClickListener {
+            view.findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard, null)
+        }
     }
 }
